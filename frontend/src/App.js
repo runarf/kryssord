04677fbd3eb41523b750ba24e95477c6d1771448
Runar <runar@tuta.io>
2021-09-09 18:18:58 +0200

    remove center

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,49 +19,54 @@ function App() {
     setWordsByCountLetters(html);
   };
   return (
-    <div
-      style={{
-        width: "120%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ width: "300px" }}>
-          <Input.Search
-            onFocus={(event) => event.target.select()}
-            allowClear
-            loading={loading}
-            size="large"
-            enterButton="Search"
-            onSearch={(value) => fetchHtml(value)}
-            onPressEnter={(event) => {
-              event.target.select();
-              fetchHtml(event.target.value);
-            }}
-          />
-        </div>
-      </div>
+    <div style={{}}>
       <div
-        style={{ display: "flex", justifyContent: "center", padding: "4px" }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          // justifyContent: "center",
+        }}
       >
-        {wordsByCountLetters.map(({ words, countLetters }) => {
-          return (
-            <div key={countLetters} style={{ padding: "4px" }}>
-              <div>
-                <div>{countLetters} </div>
-              </div>
-              <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{}}>
+            <Input.Search
+              onFocus={(event) => event.target.select()}
+              allowClear
+              loading={loading}
+              size="large"
+              enterButton="Search"
+              onSearch={(value) => fetchHtml(value)}
+              onPressEnter={(event) => {
+                event.target.select();
+                fetchHtml(event.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            // justifyContent: "center",
+            padding: "4px",
+          }}
+        >
+          {wordsByCountLetters.map(({ words, countLetters }) => {
+            return (
+              <div key={countLetters} style={{ padding: "4px" }}>
                 <div>
-                  {words.map((word) => (
-                    <div>{word}</div>
-                  ))}
+                  <div>{countLetters} </div>
+                </div>
+                <div>
+                  <div>
+                    {words.map((word) => (
+                      <div>{word}</div>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
